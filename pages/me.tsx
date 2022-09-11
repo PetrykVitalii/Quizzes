f097764336a4aa675461b8099c7cb5c522a0baf9
Vitalii <petruk.v.m2@gmail.com>
2022-09-11 12:57:09 +0300

    profile container

--- pages/me.tsx
+++ pages/me.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 interface Props {}
 
@@ -6,9 +7,30 @@ const Profile: React.FC<Props> = () => {
   console.log('me');
 
   return (
-    <h1>me</h1>
+    <ProfileStyled>
+      <ProfileContainer>
+      </ProfileContainer>
+    </ProfileStyled>
   );
 };
+
+const ProfileContainer = styled.div`
+  max-width: 800px;
+  width: 100%;
+  padding: 24px 40px;
+  border-radius: 16px;
+  background-color: ${({ theme }) => theme.colors.Second};
+  border: ${({ theme }) => `1px solid ${theme.colors.Main}`};
+  position: relative;
+`;
+
+const ProfileStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 24px;
+  flex: 1;
+`;
 
 export async function getStaticProps() {
   return {
